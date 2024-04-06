4c98c0270c67f0e4f66d8b71ff1e9dce98ab6772
Oiurin <o.iurin@artsofte.ru>
2024-04-06 15:09:47 +0500

    add destroy service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app/app.component';
-import { CabinetRoutingModule } from './children/cabinet/cabinet.routing-module';
-import { NotFoundRoutingModule } from './children/not-found/not-found.routing-module';
+import { DestroyService } from './services/destroy.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +12,9 @@ import { NotFoundRoutingModule } from './children/not-found/not-found.routing-mo
     BrowserModule,
     AppRoutingModule,
   ],
-  providers: [],
+  providers: [
+    DestroyService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/children/cabinet/children/dashboard/pages/layout-dashboard/layout-dashboard.page.ts
+++ src/app/children/cabinet/children/dashboard/pages/layout-dashboard/layout-dashboard.page.ts
@@ -3,7 +3,8 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
     templateUrl: './layout-dashboard.page.html',
     styleUrls: ['./styles/layout-dashboard.page.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    providers: []
 })
 export class LayoutDashboardPage {
 
